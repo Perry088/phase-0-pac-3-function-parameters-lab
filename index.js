43c01558f790ada77a8710595e38ada94c27bcc9
Trenton Perry <trentperry08@gmail.com>
2023-08-03 18:28:24 -0800

    Finished

---
+++ index.js
@@ -0,0 +1,7 @@
+function introduction(Trent) {return (`Hi, my name is ${Trent}.`)}
+
+console.log(introduction(Trent))
+
+function introductionWithLanguage(Trent, JavaScript) {return (`Hi, my name is ${Trent} and I am learning to program in ${JavaScript}.`)}
+
+function introductionWithLanguageOptional(Trent, language ="JavaScript") {return `Hi, my name is ${Trent} and I am learning to program in ${language}.`}
